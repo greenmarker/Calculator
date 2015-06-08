c43a804e37a89c9555f5c1d7c3e5ba277a1d0c27
Marek Gierat <gieratm@gmail.com>
2015-06-08 08:43:10 +0200

    More tests: subtract, multiply, divide, 1/x, sqrt

--- calc/src/test/java/com/github/greenmarker/calc/controller/CalculatorTest.java
+++ calc/src/test/java/com/github/greenmarker/calc/controller/CalculatorTest.java
@@ -13,6 +13,21 @@ import static junit.framework.Assert.*;
 public class CalculatorTest {
 
     @Test
+    public void changeSign(){
+        Calculator calc = new Calculator();
+        CalcResult res;
+
+        res = calc.processKeypadInput(KeypadButton.ONE);
+        assertEquals("Wrong userInput after entering 1", "1", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.SIGN);
+        assertEquals("Wrong userInput after first changing sign", "-1", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.SIGN);
+        assertEquals("Wrong userInput after second changing sign", "1", res.userInput);
+    }
+
+    @Test
     public void add(){
         Calculator calc = new Calculator();
         CalcResult res;
@@ -31,17 +46,105 @@ public class CalculatorTest {
     }
 
     @Test
-    public void changeSign(){
+    public void subtract(){
         Calculator calc = new Calculator();
         CalcResult res;
 
-        res = calc.processKeypadInput(KeypadButton.ONE);
-        assertEquals("Wrong userInput after entering 1", "1", res.userInput);
+        res = calc.processKeypadInput(KeypadButton.THREE);
+        assertEquals("Wrong userInput after 1st operation", "3", res.userInput);
 
-        res = calc.processKeypadInput(KeypadButton.SIGN);
-        assertEquals("Wrong userInput after first changing sign", "-1", res.userInput);
+        res = calc.processKeypadInput(KeypadButton.MINUS);
+        assertEquals("Wrong userInput after 2nd operation", "3", res.userInput);
 
-        res = calc.processKeypadInput(KeypadButton.SIGN);
-        assertEquals("Wrong userInput after second changing sign", "1", res.userInput);
+        res = calc.processKeypadInput(KeypadButton.FIVE);
+        assertEquals("Wrong userInput after 3rd operation", "5", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.CALCULATE);
+        assertEquals("Wrong userInput after 4th operation", "-2", res.userInput);
     }
+
+    @Test
+    public void multiply(){
+        Calculator calc = new Calculator();
+        CalcResult res;
+
+        res = calc.processKeypadInput(KeypadButton.FOUR);
+        assertEquals("Wrong userInput after 1st operation", "4", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.MULTIPLY);
+        assertEquals("Wrong userInput after 2nd operation", "4", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.SIX);
+        assertEquals("Wrong userInput after 1st operation", "6", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.CALCULATE);
+        assertEquals("Wrong userInput after 4th operation", "24", res.userInput);
+    }
+
+    public void divide(){
+        Calculator calc = new Calculator();
+        CalcResult res;
+
+        res = calc.processKeypadInput(KeypadButton.FIVE);
+        assertEquals("Wrong userInput after 1st operation", "5", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.DIV);
+        assertEquals("Wrong userInput after 2nd operation", "5", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.TWO);
+        assertEquals("Wrong userInput after 1st operation", "2", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.CALCULATE);
+        assertEquals("Wrong userInput after 4th operation", "2,5", res.userInput);  // TODO , - it's locale sensitive!
+    }
+
+    public void decimalSeparator(){
+        Calculator calc = new Calculator();
+        CalcResult res;
+
+        res = calc.processKeypadInput(KeypadButton.EIGHT);
+        assertEquals("Wrong userInput after 1st operation", "6", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.DECIMAL_SEP);
+        assertEquals("Wrong userInput after 1st operation", "6", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.ONE);
+        assertEquals("Wrong userInput after 1st operation", "6,1", res.userInput); // TODO , - it's locale sensitive!
+
+        // second DECIMAL_SEP is ignored
+
+        res = calc.processKeypadInput(KeypadButton.DECIMAL_SEP);
+        assertEquals("Wrong userInput after 1st operation", "6,1", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.TWO);
+        assertEquals("Wrong userInput after 1st operation", "6,12", res.userInput); // TODO , - it's locale sensitive!
+    }
+
+    // 7
+
+    /**
+     * 1/x
+     */
+    public void multiplicativeInverse(){
+        Calculator calc = new Calculator();
+        CalcResult res;
+
+        res = calc.processKeypadInput(KeypadButton.EIGHT);
+        assertEquals("Wrong userInput after 1st operation", "8", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.RECIPROC);
+        assertEquals("Wrong userInput after 2nd operation", "0,125", res.userInput);
+    }
+
+    public void sqrt() {
+        Calculator calc = new Calculator();
+        CalcResult res;
+
+        res = calc.processKeypadInput(KeypadButton.NINE);
+        assertEquals("Wrong userInput after 1st operation", "9", res.userInput);
+
+        res = calc.processKeypadInput(KeypadButton.SQRT);
+        assertEquals("Wrong userInput after 2nd operation", "3", res.userInput);
+    }
+
 }
